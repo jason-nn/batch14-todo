75e426cecdc58a61ac3a042784d00fba7b67736d
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-12 10:45:28 +0800

    Add create, update, and delete for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,47 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.all
+    @categories = Category.all.order(:id)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Created new category'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Updated category'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path, status: :see_other, notice: 'Deleted category'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
