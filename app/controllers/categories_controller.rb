e206df446764d396a4a4da32c4c5b3c786f8593b
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-12 11:02:53 +0800

    Add user id to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,17 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, only: %i[edit update destroy]
 
   def index
-    @categories = Category.all.order(:id)
+    @categories = current_user.categories.order(:id)
   end
 
   def new
-    @category = Category.new
+    @category = current_user.categories.new
   end
 
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.new(category_params)
 
     if @category.save
       redirect_to categories_path, notice: 'Created new category'
@@ -19,13 +20,9 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @category = Category.find(params[:id])
-
     if @category.update(category_params)
       redirect_to categories_path, notice: 'Updated category'
     else
@@ -34,7 +31,6 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
 
     redirect_to categories_path, status: :see_other, notice: 'Deleted category'
@@ -44,5 +40,9 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
   end
 end
