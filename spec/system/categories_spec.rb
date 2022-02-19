06685f63f96e9512d7fe3625089812f6cd45386f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-19 09:31:39 +0800

    Add category system spec

--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -14,7 +14,34 @@ RSpec.describe 'Categories System Spec', type: :system do
     @task = @category.tasks.create(name: @task_name, deadline: @task_deadline)
   end
 
+  before :each do
+    sign_in @user
+    visit categories_path
+  end
+
   after :all do
     @user.destroy
+  end
+
+  it 'creating category' do
+    click_on '􀅼'
+    expect(page).to have_content('New category')
+    fill_in 'Name', with: 'Doing'
+    click_on 'Create category'
+    expect(page).to have_content('Doing')
+  end
+
+  it 'updating category' do
+    click_on '􀈊'
+    expect(page).to have_content('Edit category')
+    fill_in 'Name', with: 'Doing'
+    click_on 'Update category'
+    expect(page).to have_content('Doing')
+  end
+
+  it 'destroying category', js: true do
+    click_on '􀈑'
+    visit categories_path
+    expect(page).not_to have_content(@category_name)
   end
 end
